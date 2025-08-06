92eef4ee1c54e8c8d8ae9456bf8e2f1fb40a8d4b
Elishanunana <nunanaelisha2004@gmail.com>
2025-08-06 10:41:33 +0000

    Revert RoomSerializer to handle raw JSON for facilities

--- core/serializers/room_serializer.py
+++ core/serializers/room_serializer.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from core.models import Room
-import json
 
 class RoomSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
@@ -9,13 +8,3 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = '__all__'
         read_only_fields = ['provider']
-
-    def validate_facilities(self, value):
-        if isinstance(value, str):
-            try:
-                value = json.loads(value)  # Parse string like "[1, 2, 3]" to list
-                if not isinstance(value, list) or not all(isinstance(i, int) for i in value):
-                    raise serializers.ValidationError("Facilities must be a list of integers.")
-            except json.JSONDecodeError:
-                raise serializers.ValidationError("Invalid facilities format. Expected a list of integers, e.g., [1, 2, 3].")
-        return value

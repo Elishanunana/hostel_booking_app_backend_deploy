01a645c1d96b774f0109ae7dfbb637e3fb9fd5f3
Elishanunana <nunanaelisha2004@gmail.com>
2025-08-08 02:00:23 -0400

    Added http://localhost:5173 to  CORS_ALLOWED_ORIGIN

--- hostel_booking/settings.py
+++ hostel_booking/settings.py
@@ -114,7 +114,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    config('FRONTEND_URL', default='http://localhost:3000'),
+    'http://localhost:5173',
+    config('FRONTEND_URL', default='http://localhost:5173'),
 ]
 
 CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
